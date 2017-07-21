789bdf913da704f6f91efad70b62ecd224b502b8
Steven Stewart <steven@woohooltd.uk>
2017-07-21 05:28:11 +0100

    Initial commit with triangle example and unit tests.

---
+++ CodeWars.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CodeWars.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CodeWars.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d15cec08-7255-4bb0-85c1-7a67a8ba451f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CodeWars/Triangle/Triangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class Triangle
+    {
+        public static bool IsTriangle(int a, int b, int c)
+        {
+            //we don't want negative values.
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                return false;
+            }
+
+            //ensure side length is greater than the third in every combination.
+            if (a + b > c && a + c > b && b + c > a)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
