771ba9015569bbc4f442bba52f6925ad587af485
Steven Stewart <steven@woohooltd.uk>
2017-09-28 10:06:26 +0100

    New codewars example for formating a phone number for a given int array.

---
+++ CodeWars.UnitTests/PhoneNumberTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace CodeWars.UnitTests
+{
+    //Trait is a way of grouping and organising tests, not required but have left it here 
+    //as a reminder.
+    [Trait("Category", "PhoneNumber")]
+    public class PhoneNumberTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }, "(123) 456-7890")]
+        [InlineData(new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, "(111) 111-1111")]
+        [InlineData(new int[] { 10, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, "")]
+        [InlineData(new int[] { 10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, "")]
+        public void FixedTest(int[] numbers, string result)
+        {
+            Assert.Equal(PhoneNumber.CreatePhoneNumber(numbers), result);
+        }
+
+    }
+}
---
+++ CodeWars/PhoneNumber/PhoneNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class PhoneNumber
+    {
+        public static string CreatePhoneNumber(int[] numbers)
+        {
+            if (numbers.Length < 10) return "";
+            if (numbers.Any(x => x > 9)) return "";
+
+            var result = "(";
+            result += numbers[0];
+            result += numbers[1];
+            result += numbers[2];
+            result += ") ";
+
+            result += numbers[3];
+            result += numbers[4];
+            result += numbers[5];
+            result += "-";
+
+            result += numbers[6];
+            result += numbers[7];
+            result += numbers[8];
+            result += numbers[9];
+
+            return result;
+        }
+    }
+}
